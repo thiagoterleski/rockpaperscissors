2ace16720c1fcdf960e6be8e34345dd52dfcbdaa
Thiago Terleski <thiagoterleski@Mac-mini-de-Thiago.local>
2017-09-17 08:39:24 -0300

    Setup the App class and tests

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,64 @@
 export default class App {
-	constructor () {}
+	/**
+	 * Define the main DOM elements for playing (stage and buttons)
+	 */
+	constructor() {
+		this.$game = document.querySelector('.game-content');
+		this.$playHumanButton = document.querySelector('.play-human-button');
+		this.$playComputerButton = document.querySelector('.play-computer-button');
+		this.createListeners();
+	}
+
+	/**
+	 * Create Listeners for buttons
+	 * @return {}
+	 */
+	createListeners() {
+		this.$playHumanButton.addEventListener("click", (event) => {
+			event.preventDefault();
+			this.play('human');
+		})
+		this.$playComputerButton.addEventListener("click", (event) => {
+			event.preventDefault();
+			this.play('computer');
+		})
+	}
+
+	/**
+	 * This is a main function for the app, once the user clicked on
+	 * button to play (as human, or computer game), this function will be responsible
+	 * for start the game.
+	 * @param  {String} [mode='human'] player mode
+	 */
+	play(mode = 'human') {
+		if (mode === 'human') {
+			console.log('human')
+		} else {
+			console.log('computer')
+		}
+	}
+
+	/**
+	 * Responsible for render the result content
+	 * @return {String} results template
+	 */
+	showResults() {
+
+	}
+
+	/**
+	 * A initial function that will be called
+	 * @return {html} template for game stage
+	 */
+	render() {
+
+		const welcome = `
+			<span class="message">
+				Choose a mode to play
+			</span>
+		`;
+
+		this.$game.innerHTML = welcome;
+	}
+
 }
--- test/app.spec.js
+++ test/app.spec.js
@@ -1,18 +1,33 @@
 import App from '../src/js/app';
 
-describe('Game class test', () => {
+describe('App class test', () => {
+
+	const game = document.createElement("div");
+	const playHumanButton = document.createElement("button");
+	playHumanButton.setAttribute("class", "button big accent play-human-button");
+	const playComputerButton = document.createElement("button");
+	playComputerButton.setAttribute("class", "button big accent play-computer-button");
+
+	game.appendChild(playHumanButton);
+	game.appendChild(playComputerButton);
+	const body = document.getElementsByTagName("body")[0];
+	body.appendChild(game);
 
 	const AppClass = new App();
 
+	it('shoude have a createListeners function', () => {
+    expect(AppClass.createListeners).to.be.a('function');
+	});
 	it('shoude have a showResults function', () => {
     expect(AppClass.showResults).to.be.a('function');
 	});
 	it('shoude have a play function', () => {
-    expect(AppClass.showResults).to.be.a('function');
+    expect(AppClass.play).to.be.a('function');
 	});
 	it('shoude have a render function', () => {
-    expect(AppClass.showResults).to.be.a('function');
+    expect(AppClass.render).to.be.a('function');
 	});
+
 
 
 });
