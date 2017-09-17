b29b15635ca2a531fdd07751159646069d3f9112
Thiago Terleski <thiagoterleski@Mac-mini-de-Thiago.local>
2017-09-17 11:18:02 -0300

    Created the app functions to handle the game actions on layout

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,6 @@
+import { Counter, loadImage } from './utils';
+import Game from './game';
+
 export default class App {
 	/**
 	 * Define the main DOM elements for playing (stage and buttons)
@@ -25,25 +28,152 @@ export default class App {
 	}
 
 	/**
+	 * This function will show an
+	 * animated counter on screen before each play
+	 */
+	showCountdown() {
+		this.$game.innerHTML = `
+			<div class="countdown">
+				<span class="text">
+					Let's go!
+				</span>
+			</div>
+		`;
+
+		const $countdown = document.querySelector('.countdown');
+
+		return new Promise((resolve) => {
+			Counter.start(3, (count) => {
+				if (count === 0) {
+					return resolve()
+				}
+				$countdown.innerHTML = `
+				<span class="number">
+					${count}
+				</span>
+				`;
+			});
+		})
+	}
+
+	/**
+	 * This function will display the results content
+	 * after the countdown finish
+	 * @param  {results} results Game compute function results
+	 */
+	showResults(results) {
+
+		const isDraw = Boolean(results === 'draw');
+
+		const getTextContent = () => {
+			if (isDraw) {
+				return 'Draw';
+			}
+			return `${winner.name === 'human' ? 'You' : winner.name} win!`;
+		}
+
+		const { winner, loser } = results;
+		const getResultClassSufix = () => {
+			const randomPosition = ['vertical','horizontal'][Math.round(Math.random())];
+			if (isDraw) {
+				return `is-draw ${randomPosition}`;
+			}
+			return `${winner.name}-wins ${randomPosition}`;
+		}
+
+		this.$game.innerHTML = `
+			<div class="results ${getResultClassSufix()}">
+				<div class="player player1">
+					<div class="player-content">
+						<img src='${loadImage(isDraw ? 'hand' : winner.choice)}' width="400" height="400" />
+					</div>
+				</div>
+				<div class="winner-text">
+					${getTextContent()}
+				</div>
+				<div class="player player2">
+					<div class="player-content">
+						<img src='${loadImage(isDraw ? 'hand' : loser.choice)}' width="400" height="400" />
+					</div>
+				</div>
+			</div>
+		`;
+
+	}
+
+	/**
 	 * This is a main function for the app, once the user clicked on
 	 * button to play (as human, or computer game), this function will be responsible
 	 * for start the game.
 	 * @param  {String} [mode='human'] player mode
 	 */
 	play(mode = 'human') {
+		const GameClass = new Game();
+
 		if (mode === 'human') {
-			console.log('human')
-		} else {
-			console.log('computer')
+
+			GameClass.createPlayer('human');
+			GameClass.createPlayer('computer');
+
+			const choicesTemplate = `
+				<div class="choices">
+					<span class="title">Your time!</span>
+					<div class="options">
+						<button type="button" class="button invisible choice-button" data-choice="paper">
+							<img src='${loadImage('paper')}' width="64" height="64" />
+						</button>
+						<button type="button" class="button invisible choice-button" data-choice="scissors">
+							<img src='${loadImage('scissors')}' width="64" height="64" />
+						</button>
+						<button type="button" class="button invisible choice-button" data-choice="rock">
+							<img src='${loadImage('rock')}' width="64" height="64" />
+						</button>
+					</div>
+				</div>
+			`;
+
+			this.$game.innerHTML = choicesTemplate;
+
+			const choiceButtons = document.querySelectorAll("button.choice-button");
+			choiceButtons.forEach($button => $button.addEventListener('click',(event) => {
+				const $elem = event.currentTarget;
+				const humanChoice = $elem.dataset.choice;
+
+				GameClass.setPlayerChoice('human', humanChoice);
+				const compChoice = GameClass.getRandChoice()
+				GameClass.setPlayerChoice('computer', compChoice);
+
+				const results = GameClass.computeGame('human', 'computer');
+
+				console.log(results)
+
+				this.showCountdown()
+					.then(() => {
+						this.showResults(results);
+				})
+
+			}));
+
 		}
-	}
+		// Computer VS Computer
+		else {
 
-	/**
-	 * Responsible for render the result content
-	 * @return {String} results template
-	 */
-	showResults() {
+			GameClass.createPlayer('computer1');
+			GameClass.createPlayer('computer2');
 
+			const comp1Choice = GameClass.getRandChoice()
+			const comp2Choice = GameClass.getRandChoice()
+
+			GameClass.setPlayerChoice('computer1', comp1Choice);
+			GameClass.setPlayerChoice('computer2', comp2Choice);
+
+			const results = GameClass.computeGame('computer1', 'computer2');
+
+			this.showCountdown()
+				.then(() => {
+					this.showResults(results);
+			})
+		}
 	}
 
 	/**
